e9fdd311d99750367bc3419bf65a075578b27e9c
Efrun Evdi <efruncetkin@gmail.com>
2024-06-17 00:22:41 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entites/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entites
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // Benzersiz olduğunu belirtmek için
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entites/Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entites
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore] // MongoDB'ye veri yazılırken veya MongoDB'den veri okunurken bu alanın yoksayılmasını sağlar
+        public Category Category { get; set; }
+    }
+}
